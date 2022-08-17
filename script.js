d31fd485855af16db708842a865829dbeb36f03f
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-08-17 14:04:18 -0500

    Add color mode and rainbow mode

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 const container = document.querySelector('#container');
 const boxes = document.querySelectorAll('.box');
+const colorMode = document.querySelector('#color-mode');
+const rainbowMode = document.querySelector('#rainbow-mode');
+const colorPicker = document.querySelector('#color');
 
 let numSquares = 20;
 
@@ -24,10 +27,35 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+let colorType = 'color';
+let color = colorPicker.value;
+
+function updateColor() {
+  color = colorPicker.value;
+}
+
+function updateFirst() {
+  color = colorPicker.value;
+}
+
+colorPicker.addEventListener('input', updateFirst, false);
+
+colorMode.addEventListener('click', function () {
+  colorType = 'color';
+});
+
+rainbowMode.addEventListener('click', function () {
+  colorType = 'rainbow';
+});
+
 function changeColor(e) {
   if (e.type === 'mouseover' && !mouseDown) return;
   if (e.target.classList.contains('box')) {
-    e.target.style.backgroundColor = `${randomRGB()}`;
+    if (colorType === 'rainbow') {
+      e.target.style.backgroundColor = `${randomRGB()}`;
+    } else if (colorType === 'color') {
+      e.target.style.backgroundColor = color;
+    }
   }
 }
 
